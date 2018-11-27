4bde7e5529bf16bb9e9a85cc1d0a18795674f9f3
mischa1998 <mischa1998@inbox.ru>
2018-11-27 17:52:38 +0300

    Update 471.py

--- 471.py
+++ 471.py
@@ -8,18 +8,12 @@ from array import array
 n = np.loadtxt("data/t7.txt", delimiter='\n', dtype=np.float)
 n1 = np.loadtxt("data/t17.txt", delimiter='\n', dtype=np.float)
 
-#m = np.loadtxt("data/x.txt", delimiter='\n', dtype=np.float)
-#m1 = np.loadtxt("data/y.txt", delimiter='\n', dtype=np.float)
 
 z = np.loadtxt("data/x17.txt", delimiter='\n', dtype=np.float)
 z1 = np.loadtxt("data/y17.txt", delimiter='\n', dtype=np.float)
 
-#l = np.loadtxt("data/xlong.txt", delimiter='\n', dtype=np.float)
-#l1 = np.loadtxt("data/ylong.txt", delimiter='\n', dtype=np.float)
-
+#построение графика точного и аналитического решения задачи в заданный момент времени
 plt.plot(n, n1, color='blue')
-#plt.plot(m, m1, color='green')
 plt.plot(z, z1, color='orange')
-#plt.plot(l, l1, color='m')
 plt.title("Неявная схема")
 plt.show()
