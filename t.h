f09508b03c9319006486186ad61a80400e184b6b
mischa1998 <mischa1998@inbox.ru>
2018-11-27 17:42:10 +0300

    Update t.h

--- t.h
+++ t.h
@@ -2,42 +2,18 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
-
+//для построения графика точного решения создаются файлы с координатами (x,y) - которые получены из точного аналитического решения
 
 using namespace std;
 double Pi  =3.141592653589793238463;
 
 double tau = 0;
 
-double func(double x)
-{
-	return exp(-4*Pi*Pi*tau) * sin(2.0*Pi*x);
-}
-
 double func1(double x)
 {
     return exp(-0.5 * tau) * sin(x);
 }
 
-
-int setting(void)
-{
-    ifstream t;
-    t.open("data/tau.txt");
-    t>>tau;
-    t.close();
-	ofstream in;
-    ofstream in1;
-    in.open("data/t.txt");
-    in1.open("data/t1.txt");
-    for(double x = 0.0; x <= 1.0; x += 0.001) {
-    	in<<x<<endl;
-    	in1<<func(x)<<endl;
-    }
-    in.close();
-    in1.close();
-	return 0;
-}
 
 int setting1(void)
 {
